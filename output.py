a7e61e4bb98d2a5b8fb5671f1623583a1d0fe69e
Abdulrahman <abd.moh.yousef@gmail.com>
2022-05-30 00:21:02 +0200

    Update output.py

--- output.py
+++ output.py
@@ -1,22 +1,32 @@
+arr={}
 M = eval(input())
 Count = 0
-print( "The Count is:", Count)
+print("The Count is:", Count)
 X = eval(input())
 while True:
     Cf = 0
-    print( "2nd Repeat")
+    print("2nd Repeat")
     while True:
-        print( "Iter")
+        print("Iter")
         Cf = Cf + 1
         if Cf == 2:
             break
     Count = Count + 1
-    print( Count)
+    print(Count)
     if Count == X:
         break
-print( "Done")
+print("Done")
 Num1 = 47293
 Num2 = 3290
 Result = Num1 + Num2
-print( "The result is:", Result)
+print("The result is:", Result)
+i = 10
+while i > 0:
+    print("Iteration" , i)
+    i = i - 1
+arr[4] = 5
+print(arr[4])
+for i in range(0,5):
+    arr[i] = i
+    print(arr[i])
 input("Press enter to exit ")
