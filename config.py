08f9b52912bc2010ac22a0c43a34689b41797e1a
Abdulrahman <abd.moh.yousef@gmail.com>
2023-02-24 22:38:26 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+
+equality = [">", "<", "="]
+operators = ['+', '-', '*', '/']
+statements = ['WHILE', 'ENDWHILE', 'FOR', 'NEXT', 'UNTIl', 'REPEAT', 'ELSE', 'REPEAT', 'IF', 'ENDIF', 'PRINT', 'INPUT']
