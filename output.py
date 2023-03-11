15db65bcf85902f5f0651f3b2ef8c3256c2c7b82
Abdulrahman <abd.moh.yousef@gmail.com>
2023-03-11 21:28:54 +0200

    Update output.py

--- output.py
+++ output.py
@@ -1,19 +1,18 @@
 import random
 import math
-arr=[[0 for i in range(1000)] for f in range(1000)]
-def sum(sum1,Num2):
-    S = sum1+Num2
-    return S
-num = 16
-Num = "Hello".upper() + " World!".upper()
-Num = Num[-1:0]
-print(Num)
-if num == 1234:
-    print("test completed")
-else:
-    print("good enough")
-arr[2][1] = 17
-arr[2][2] = eval(input())
-print(arr)
-print(sum(arr[2][1],arr[2][2]))
+t=[[0 for i in range(1000)] for f in range(1000)]
+avg=[0 for i in range(1000)]
+n=[[0 for i in range(1000)] for f in range(1000)]
+x = [[5,1,2],[1,6,3]]
+t=[0,0]
+avg=[0,0]
+for c1 in range(0,2):
+    for c2 in range(0,3):
+        t[c1]=t[c1] + x[c1][c2]
+    avg[c1] = t[c1]/3
+    print(avg[c1])
+n[1][2]=3
+print((n[1][2]))
+if x != 3 :
+    print("NO")
 input("Press enter to exit ")
